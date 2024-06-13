cdc1f820b420eb6e98a143638fc3d5505c924665
Sonia Nikiel <sonia.nikiel1.programista@gmail.com>
2024-06-13 20:38:36 +0200

    Add registered user to file

--- KsiazkaAdresowa.hpp
+++ KsiazkaAdresowa.hpp
@@ -10,13 +10,19 @@ class KsiazkaAdresowa
     int idZalogowanegoUzytkownika;
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
+    string nazwaPlikuZUzytkownikami;
     
     vector <Uzytkownik> uzytkownicy;
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
     bool czyIstniejeLogin(string login);
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    string konwerjsaIntNaString(int liczba);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
     
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
     
